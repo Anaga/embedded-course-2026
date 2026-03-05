904a8046c3d90d95edeb37466fdcf87adf2d0f6c
N4thanT <nathanstaryu@gmail.com>
2026-03-05 19:52:34 +0200

    Lesson 6 classroom task.

--- module2/lesson06_classes_objects_encapsulation/dots/dots/include/dots.h
+++ module2/lesson06_classes_objects_encapsulation/dots/dots/include/dots.h
@@ -4,13 +4,41 @@
 class point
 {
 private:
-    uint8_t x;
+    uint8_t x; 
     uint8_t y;
-
 public:
-    point();  // Empty const 
-    point(uint8_t X_val, uint8_t Y_val); //const with loc
-    ~point();
+    point(); // empty const (constructor)
+    point(uint8_t X, uint8_t Y); // constructor with location | spesific cords
+    ~point(); // destructor
     void print();
     uint8_t distance(point A, point B);
 };
+
+point::point(/* args(arguments) */) // random cords one | :: <- shows that something(thing after ::) is in the class
+{
+    Serial.printf("Point instance constructor call \n");
+}
+
+point::point(uint8_t X, uint8_t Y) // spesific cords inside class showy thingy
+{
+    Serial.printf("Point instance constructor call, x%d, y%d \n", X, Y);
+    this->x = X;
+    this->y = Y;
+}
+
+point::~point() // destructor
+{
+        Serial.printf("Point destructor call \n");
+}
+
+void point::print()
+{
+    Serial.printf("Point loc: x%d, y%d \n", x, y);
+}
+
+uint8_t point::distance(point A, point B)
+{
+    uint8_t delta_x = A.x - B.y;
+    uint8_t delta_y = A.x - B.y;
+    return delta_x + delta_y;
+}
--- module2/lesson06_classes_objects_encapsulation/dots/dots/src/main.cpp
+++ module2/lesson06_classes_objects_encapsulation/dots/dots/src/main.cpp
@@ -7,46 +7,34 @@
 // Setup
 // ============================================================
 #define SERIAL_SPEED 115200
-#define STARTUP_DELAY 3500
+#define STARTUP_DELAY 1500
 
 void setup() {
     Serial.begin(SERIAL_SPEED);
     delay(STARTUP_DELAY);
     Serial.println(" ================================================= ");
-    Serial.println("| Lesson 6 Demo  |");
+    Serial.println("|  Lesson 6 Demo  |");
     Serial.println(" ================================================= ");
 
-
-    delay(5000); 
+    delay(5000);
     point point_zero;
-    Serial.println("point_zero created");
-    delay(50);
-
     point_zero.print();
 
     point point_one(5,10);
-
-    Serial.println("point_one created");
-    delay(50);
     point_one.print();
 
-    Serial.println("get distance A");
     uint8_t distA = point_one.distance(point_zero, point_one);
-    Serial.printf("distA is: %d \n", distA);
-    delay(50);
 
-    Serial.println("get distance B");
     uint8_t distB = point_zero.distance(point_one, point_zero);
+    Serial.printf("distA is: %d \n", distA);
     Serial.printf("distB is: %d \n", distB);
-    delay(50);
-
 }
 
 void loop() {
-            Serial.println("Tick");
-            delay(500); 
+  // put your main code here, to run repeatedly:
+  Serial.println("Tick");
+  delay(500);
 
-            Serial.println("Tack");
-            delay(500); 
-        }
-
+  Serial.println("Tack");
+  delay(500);
+}
