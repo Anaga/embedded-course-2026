2abeeba9e5707728e1487ed5f8a3267ff367cf91
Aleksandr Bobyrev <Aleksandr.Bobyrev@stoneridge.com>
2026-03-06 20:49:54 +0200

    Add class filess

---
+++ module2/lesson07_inheritance_templates_datastructures/Class_files/dots.h
@@ -0,0 +1,18 @@
+#include <stdint.h>
+#include <Arduino.h>
+
+class point
+{
+private:
+    uint8_t x;
+    uint8_t y;
+
+public:
+    point();  // Empty const 
+    point(uint8_t X_val, uint8_t Y_val); //const with loc
+    ~point();
+    void print();
+    uint8_t distance(point A, point B);
+    void move_x();
+    void move_y();
+};
